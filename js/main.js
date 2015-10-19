08690b1d7ebae275272fdd34688f0aab45246d0d
Yuriy Shefer <sheff@sheffmachine.com>
2015-10-18 19:03:05 -0500

    scroll enabled

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 //Global Variables
 var imgArray = ['url(img/content-img.jpg', 'url(img/bike.jpg)', 'url(img/bike1.jpg)', 'url(img/bike2.jpg)'];
-var index = 0;
+	var index = 0;
 
 //Event Handlers
 $('.arrow-left').on('click', prevImg);
 $('.arrow-right').on('click', nextImg);
 
-//Functions
+//Image manipulation
 function nextImg(){
 	if (index < imgArray.length - 1){
 		index++;
@@ -29,3 +29,38 @@ function prevImg(){
 	}
 
 }
+
+//Smooth Scrolling
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
+
+//Active Class Switching
+$(window).scroll(function() {
+    var windscroll = $(window).scrollTop();
+    if (windscroll >= 701) {
+        $('.slide').each(function(i) {
+            if ($(this).position().top <= windscroll - 20) {
+                $('nav a.active').removeClass('active');
+                $('nav a').eq(i + 1).addClass('active');
+            }
+        });
+
+    } else {
+
+        $('nav').removeClass('fixed');
+        $('nav a.active').removeClass('active');
+        $('nav a:nth-child(2)').addClass('active');
+    }
+
+}).scroll();
+
+//Click on link to make active
+$('#nav a').click(function() {
+    $('a').removeClass('active');
+    $(this).addClass('active');
+});
+
